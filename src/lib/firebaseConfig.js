a1b738e1e46b3f6fab939f3df622e5cf8ec0dc3a
Rakean Gilang <rgilangdp@gmail.com>
2022-02-28 17:45:48 +0700

    update

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAigzpNslZ_2JiCn4RoHLojk3Xjc5TJO2s",
+    authDomain: "realtime-todos-9047a.firebaseapp.com",
+    projectId: "realtime-todos-9047a",
+    storageBucket: "realtime-todos-9047a.appspot.com",
+    messagingSenderId: "321991122388",
+    appId: "1:321991122388:web:f43f7344ce87442c474a41",
+    measurementId: "G-G0WX880CS2"
+};
---
+++ src/routes/index.svelte
@@ -0,0 +1,107 @@
+<script>
+    import { initializeApp } from "firebase/app";
+    import {
+        getFirestore,
+        collection,
+        onSnapshot,
+        doc,
+        addDoc,
+        updateDoc,
+        deleteDoc,
+    } from "firebase/firestore";
+    import { firebaseConfig } from "$lib/firebaseConfig";
+    // import { browser } from "$app/env";
+
+    // const firebaseApp =
+    //     browser &&
+    //     (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
+    // const db = browser && getFirestore();
+
+    const firebaseApp = initializeApp(firebaseConfig);
+
+    const db = getFirestore();
+
+    const colRef = collection(db, "todos");
+
+    let todos = [];
+
+    const unsubscribe = onSnapshot(colRef, (querySnapshot) => {
+        let fbTodos = [];
+        querySnapshot.forEach((doc) => {
+            let todo = { ...doc.data(), id: doc.id };
+            fbTodos = [todo, ...fbTodos];
+        });
+
+        todos = fbTodos;
+    });
+
+    console.log({ firebaseApp, db });
+
+    let task = "";
+    let error = "";
+
+    const addTodo = async () => {
+        if (task != "") {
+            const docRef = await addDoc(collection(db, "todos"), {
+                task: task,
+                isComplete: false,
+                createdAt: new Date(),
+            });
+            error = "";
+        } else {
+            error = "Task is empty";
+        }
+        task = "";
+    };
+
+    const markAsCompleted = async (item) => {
+        await updateDoc(doc(db, "todos", item.id), {
+            isComplete: !item.isComplete,
+        });
+    };
+
+    const deleteTodo = async (id) => {
+        await deleteDoc(doc(db, "todos", id));
+    };
+
+    const keyIsPressed = (event) => {
+        if (event.key === "Enter") {
+            addTodo();
+        }
+    };
+
+    // $: console.table(todos);
+</script>
+
+<input type="text" placeholder="New task...." bind:value={task} />
+<button on:click={addTodo}>Add</button>
+
+<ol>
+    {#each todos as item}
+        <!-- class complete akan berlaku ketika isComplete true -->
+        <li class:complete={item.isComplete}>
+            <span>
+                {item.task}
+            </span>
+            <span>
+                <button on:click={() => markAsCompleted(item)}>✓</button>
+                <button on:click={() => deleteTodo(item.id)}>✖</button>
+            </span>
+        </li>
+    {:else}
+        <p>No todos yet.</p>
+    {/each}
+    <p class="error">{error}</p>
+</ol>
+
+<svelte:window on:keydown={keyIsPressed} />
+
+<style>
+    .complete {
+        text-decoration: line-through;
+    }
+
+    .error {
+        color: red;
+    }
+</style>
